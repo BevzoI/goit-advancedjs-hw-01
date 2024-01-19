5d84cebebf37e0fbc677d03e508c89a309a364a0
BevzoI <bevzo.i.n@gmail.com>
2024-01-19 08:35:21 +0100

    uppdate task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,18 @@
-const Vimeo = require('@vimeo/player');
-const throttle = require('lodash.throttle');
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
 const player = new Vimeo(document.getElementById('vimeo-player'));
 
-
 const saveCurrentTime = throttle(() => {
-    player.getCurrentTime().then(currentTime => {
-        localStorage.setItem('videoplayer-current-time', currentTime);
-    });
-}, 1000); 
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
 player.on('timeupdate', saveCurrentTime);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const savedTime = localStorage.getItem('videoplayer-current-time');
-    if (savedTime) {
-        player.setCurrentTime(parseFloat(savedTime));
-    }
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
 });
-
